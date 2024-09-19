9cd1d415c6350b7c12349d3da7e71c981877b4dc
Brad Root <bradroot@me.com>
2024-09-18 22:28:52 -0700

    Initial Commit

---
+++ Sprioc/SpriocApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriocApp.swift
+//  Sprioc
+//
+//  Created by Brad Root on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SpriocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
